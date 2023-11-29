d5958d29b2ec112c4604bed6d6a0d0d03bd89cfa
Sk Alijan <alizaan8695@gmail.com>
2023-11-29 09:56:47 +0530

    redux install

---
+++ frontend-with-reactjs/src/Routes/MainRouter.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+const MainRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element="" />
+      <Route path="/login" element="" />
+      <Route path="/signup" element="" />
+      <Route path="*" element="" />
+    </Routes>
+  );
+};
+
+export default MainRouter;
